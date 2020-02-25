6106a74ee8b5ce321d3d1cb5242567fce8527079
Luka Dogandzic <dogandzic97@gmail.com>
2020-02-25 08:30:41 +0100

    Commit Staged

---
+++ OIWEB/Controllers/HomeController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OIWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        ObrazovniInformatorDataContext oi = new ObrazovniInformatorDataContext();
+        public ActionResult Index()
+        {
+            var clanci = from c in oi.Clancis
+                         orderby c.Datum
+                         select c;
+            
+            List<ClanciTxt> text= (from t in oi.ClanciTxts
+                                   select t).ToList();
+            ViewBag.Tekstovi = text;
+
+            return View(clanci.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            List<SpecificnostiBudzetskihKorisnika> specifikacijeBK = (from s in oi.SpecificnostiBudzetskihKorisnikas
+                                  select s).ToList();
+            ViewBag.SpecificnostiBudzetskihKorisnika = specifikacijeBK;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection fc)
+        {
+            Clanci c = new Clanci();
+            Korisnik k = (Korisnik)Session["Korisnik"];
+            c.IDKorisnik = k.IDKorisnik;
+            c.IDSBK = Convert.ToInt32(fc["IDSBK"]);
+            c.Datum = fc["Datum"];
+            c.Tip = fc["Tip"];
+
+            try {
+                oi.Clancis.InsertOnSubmit(c);
+                oi.SubmitChanges();
+            }
+            catch(Exception ex)
+            {
+                List<SpecificnostiBudzetskihKorisnika> specifikacijeBK = (from s in oi.SpecificnostiBudzetskihKorisnikas
+                                                                          select s).ToList();
+                ViewBag.SpecificnostiBudzetskihKorisnika = specifikacijeBK;
+                ViewBag.Greska = ex.Message;
+                return View();
+            }
+
+            int id = (from o in oi.Clancis
+                      select o.IDClanak).Max();
+            ClanciTxt cTxt = new ClanciTxt();
+            cTxt.IDClanak = id;
+            cTxt.Naslov = fc["Naslov"];
+            cTxt.Tekst = fc["Tekst"];
+            try
+            {
+                oi.ClanciTxts.InsertOnSubmit(cTxt);
+                oi.SubmitChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch(Exception ex)
+            {
+                List<SpecificnostiBudzetskihKorisnika> specifikacijeBK = (from s in oi.SpecificnostiBudzetskihKorisnikas
+                                                                          select s).ToList();
+                ViewBag.SpecificnostiBudzetskihKorisnika = specifikacijeBK;
+                ViewBag.Greska = ex.Message;
+                return View();
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ClanciTxt tekst = (from t in oi.ClanciTxts
+                               where t.IDClanak == id
+                               select t).Single();
+            try {
+                oi.ClanciTxts.DeleteOnSubmit(tekst);
+                oi.SubmitChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+
+            Clanci clanak = (from c in oi.Clancis
+                             where c.IDClanak == id
+                             select c).Single();
+            try
+            {
+                oi.Clancis.DeleteOnSubmit(clanak);
+                oi.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Clanci clanak = (from c in oi.Clancis
+                             where c.IDClanak == id
+                             select c).Single();
+
+            ClanciTxt tekst= (from c in oi.ClanciTxts
+                              where c.IDClanak == clanak.IDClanak
+                              select c).Single();
+            ViewBag.ClanciTxt = tekst;
+            List<SpecificnostiBudzetskihKorisnika> specifikacijeBK = (from s in oi.SpecificnostiBudzetskihKorisnikas
+                                                                      select s).ToList();
+            ViewBag.SpecificnostiBudzetskihKorisnika = specifikacijeBK;
+            return View(clanak);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id,FormCollection fc)
+        {
+            Clanci c = (from cl in oi.Clancis
+                        where cl.IDClanak == id
+                        select cl).Single();
+
+            Korisnik k = (Korisnik)Session["Korisnik"];
+            c.IDKorisnik = k.IDKorisnik;
+            c.IDSBK = Convert.ToInt32(fc["IDSBK"]);
+            c.Datum = fc["Datum"];
+            c.Tip = fc["Tip"];
+
+            try
+            {
+              oi.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                List<SpecificnostiBudzetskihKorisnika> specifikacijeBK = (from s in oi.SpecificnostiBudzetskihKorisnikas
+                                                                          select s).ToList();
+                ViewBag.SpecificnostiBudzetskihKorisnika = specifikacijeBK;
+                ViewBag.Greska = ex.Message;
+                return View();
+            }
+
+            ClanciTxt cTxt = new ClanciTxt();
+            cTxt.Naslov = fc["Naslov"];
+            cTxt.Tekst = fc["Tekst"];
+            try
+            {
+                oi.SubmitChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                List<SpecificnostiBudzetskihKorisnika> specifikacijeBK = (from s in oi.SpecificnostiBudzetskihKorisnikas
+                                                                          select s).ToList();
+                ViewBag.SpecificnostiBudzetskihKorisnika = specifikacijeBK;
+                ViewBag.Greska = ex.Message;
+                return View();
+            }
+        }
+
+        public ActionResult Proba()
+        {
+            return View();
+        }
+    }
+}
---
+++ OIWEB/Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OIWEB.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        ObrazovniInformatorDataContext oi = new ObrazovniInformatorDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection fc)
+        {
+            string username = "";
+            string lozinka = "";
+
+            if (fc["KorisnickoIme"] != null && fc["KorisnickoIme"] != "")
+            {
+                 username = fc["KorisnickoIme"];
+            }
+            else
+            {
+                ViewBag.Message = "Молим вас да унесете своје корисничко име";
+                return View();
+            }
+
+
+            if (fc["Lozinka"] != null && fc["Lozinka"] != "")
+            {
+                 lozinka = fc["Lozinka"];
+            }
+            else
+            {
+                ViewBag.Message = "Молим вас да унесете своју лозинку";
+                return View();
+            }
+
+
+            Korisnik korisnik;
+            try
+            {
+                 korisnik = (from k in oi.Korisniks
+                                     where k.KorisnickoIme.Equals(username) & k.Lozinka.Equals(lozinka)
+                                     select k).Single();
+            }
+            catch
+            {
+                ViewBag.Message = "Особа са датим корисничким именом не постоји, покушајте поново";
+                return View();
+
+            }
+
+            if (korisnik != null)
+            {
+
+                Session["Korisnik"] = korisnik;
+                return RedirectToAction("../Home/Index");
+
+            }
+            else
+            {
+                ViewBag.Message = "Покушајте поново";
+                return View();
+
+            }
+        }
+    }
+}
